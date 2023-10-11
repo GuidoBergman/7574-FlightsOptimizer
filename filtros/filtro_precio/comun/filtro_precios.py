773c3f8050126ffa0e32b30c71da5b5d6e14e8e7
Luis Waldman <luiswaldman@gmail.com>
2023-10-11 18:55:21 -0300

    Filtro precios se guarda en archivos

--- filtros/filtro_precio/comun/filtro_precios.py
+++ filtros/filtro_precio/comun/filtro_precios.py
@@ -1,6 +1,8 @@
 import logging
+import os
 from math import log
 import signal
+import struct
 
 from manejador_colas import ManejadorColas
 from modelo.estado import Estado
@@ -17,17 +19,21 @@ class FiltroPrecios:
        signal.signal(signal.SIGTERM, self.sigterm_handler)
        self._protocolo = ProtocoloFiltroPrecio()
        self._protocoloResultado = ProtocoloResultadosServidor()
-
-       self.precios_por_trayecto = {}
-       self.promedio_por_trayecto = {}
-       self.promedio = 0.0
-       self.cantidad = 0
-       
-       self.corriendo = True
-
        self._id = id
+       self.inicialar()   
+       self.corriendo = True
+       
+    def inicialar(self):
+       self.archivos_por_trayecto = {}
+       self.vuelos_por_trayecto = {}
+       self.total_por_trayecto = {}
+       self.promedio = 0.0
+       self.cantidad = 0    
+
         
     def sigterm_handler(self, _signo, _stack_frame):
+        self.cerrar_archivos()
+        self.borrar_archivos()
         self._protocolo.parar()
         logging.info('action: sigterm_received')
 
@@ -37,8 +43,7 @@ class FiltroPrecios:
     def agregar_promedio(self, promedio: float, cantidad: int):
         parte_actual = self.cantidad / (self.cantidad + cantidad)
         parte_nueva = cantidad / (self.cantidad + cantidad)
-        npromedio = (self.promedio * parte_actual) + (promedio * parte_nueva)
-        
+        npromedio = (self.promedio * parte_actual) + (promedio * parte_nueva)        
         self.promedio = npromedio
         self.cantidad += cantidad
         
@@ -47,45 +52,63 @@ class FiltroPrecios:
         
         logging.debug(f'Inicio el proceso origen { vuelo.origen } precio { vuelo.precio }')
         trayecto = f'{vuelo.origen}-{vuelo.destino}'
-        if trayecto not in self.precios_por_trayecto:
+        if trayecto not in self.archivos_por_trayecto:
             # Si el trayecto no existe en el diccionario, creamos una lista con el precio actual
-            self.precios_por_trayecto[trayecto] = [vuelo.precio]
-            self.promedio_por_trayecto[trayecto] = vuelo.precio  # Inicializamos el promedio con el primer precio
+            archivo = open(trayecto, 'ab')
+            self.archivos_por_trayecto[trayecto] = open(trayecto, 'ab')
+            self.total_por_trayecto[trayecto] = vuelo.precio
+            self.vuelos_por_trayecto[trayecto] = 1
         else:
-            # Si el trayecto ya existe en el diccionario, simplemente agregamos el precio a la lista
-            self.precios_por_trayecto[trayecto].append(vuelo.precio)
-            # Calculamos el nuevo promedio basado en el valor actual, la cantidad de elementos y el nuevo valor
-            n = len(self.precios_por_trayecto[trayecto])
-            self.promedio_por_trayecto[trayecto] = (self.promedio_por_trayecto[trayecto] * ((n - 1) / n)) + (vuelo.precio / n)
-            logging.debug(f'trayecto {trayecto} promedio calculado {self.promedio_por_trayecto[trayecto]}')
-            
-        logging.debug(f'Procesado el trayecto { trayecto } precio { vuelo.precio }')
+            archivo = self.archivos_por_trayecto[trayecto]
+            self.total_por_trayecto[trayecto] += vuelo.precio  
+            self.vuelos_por_trayecto[trayecto] += 1              
+
+        # Escribe el precio en el archivo en formato binario
+        precio_binario = struct.pack('f', vuelo.precio)
+        archivo.write(precio_binario)
         
  
+    def cerrar_archivos(self):
+        for trayecto, archivo in self.archivos_por_trayecto.items():
+            archivo.close()
+
+    def borrar_archivos(self):
+        for trayecto, archivo in self.archivos_por_trayecto.items():
+            os.remove(trayecto)
+            
     def procesar_finvuelo(self):        
         logging.info(f'Calculo el promedio y total')
-        for trayecto, precios in self.precios_por_trayecto.items():
-            self.agregar_promedio(self.promedio_por_trayecto[trayecto], len(precios))
-            
+        self.cerrar_archivos()
+        for trayecto, archivo in self.archivos_por_trayecto.items():
+            self.agregar_promedio(self.total_por_trayecto[trayecto] / self.vuelos_por_trayecto[trayecto], self.vuelos_por_trayecto[trayecto])
         self._protocolo.enviar_promedio(self.promedio, self.cantidad)
-        self._protocolo.parar_vuelos()
+        
         
     def procesar_promediogeneral(self, promedio):
         logging.info(f"Envia resultados para el promedio {promedio}")
         
-        for trayecto, precios in self.precios_por_trayecto.items():
-                        
-            precios_por_encima = [precio for precio in precios if precio > promedio]
+        for trayecto, vuelos in self.vuelos_por_trayecto.items():
             
+            logging.info(f"Abro archivos para trayecto {trayecto}")                 
+            with open(trayecto, 'rb') as archivo:
+                precios_por_encima = []
+                precio_binario = archivo.read(4)  # Leer 4 bytes a la vez (tamaño de un float)
+                while precio_binario:
+                    precio = struct.unpack('f', precio_binario)[0]
+                    if precio > promedio:
+                        precios_por_encima.append(precio)
+                    precio_binario = archivo.read(4)
+
             # Calcula el precio promedio solo para los precios por encima de 'promedio'
             if precios_por_encima:
                 precio_promedio = sum(precios_por_encima) / len(precios_por_encima)
                 precio_maximo = max(precios_por_encima)
                 res = ResultadoEstadisticaPrecios(trayecto, precio_promedio, precio_maximo)
-                logging.debug(f"Filtro enviando resultado: {trayecto} promedio: {precio_promedio}")
+                logging.info(f"Filtro enviando resultado: {trayecto} promedio: {precio_promedio}")
                 self._protocoloResultado.enviar_resultado_filtro_precio(res)
             
         self._protocoloResultado.enviar_fin_resultados_filtro_precio()
+        self.borrar_archivos()
 
         
 
